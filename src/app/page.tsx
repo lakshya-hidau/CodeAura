1bea5b02a9afdb04cd5a59e46359f136eafc11e7
Lakshya Hidau <lakshyahidau2005@gmail.com>
2025-10-17 16:00:53 +0530

    02: database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { prisma } from "@/lib/db";
 
-export default function Home() {
-  return (
-    <div>
-      <Button>
-        Click Me
-      </Button>
-    </div>
+const Page = async () => {
+  const users = await prisma.post.findMany();
     
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold mb-6">Posts</h1>
+      <ul className="space-y-4">
+        {users.map((post) => (
+          <li key={post.id} className="p-4 border rounded-lg shadow-sm">
+            <h2 className="text-xl font-semibold">{post.title}</h2>
+            <p className="mt-2 text-gray-600">{post.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
-}
+};
+
+export default Page;
