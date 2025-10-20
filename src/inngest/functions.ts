0fc49a123af66dec5cb690f3a907a4e58fe26305
Lakshya Hidau <lakshyahidau2005@gmail.com>
2025-10-20 22:54:21 +0530

    06: E2B Sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,20 +1,32 @@
 import { gemini, createAgent } from "@inngest/agent-kit";
-// import { createAgent, anthropic } from "@inngest/agent-kit";
+import { Sandbox } from "@e2b/code-interpreter";
 
 import { inngest } from "./client";
+import { getSendbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("codeaura-nextjs-test-2");
+      return sandbox.sandboxId;
+    });
+
     const codeAgent = createAgent({
     name: "code-agent", 
     system: "You are an expert next.js developer who writes clean, efficient, and readable code. You write simple next.js and React code snippets.",
     model: gemini({ model: "gemini-2.0-flash" })
     });
 
-    const { output } = await codeAgent.run('Summarize the following text: ' + event.data.value);
+    const { output } = await codeAgent.run('Write the following snippets: ' + event.data.value);
     
-    return { output };
+    const sandboxURL = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSendbox(sandboxId);
+      const host =  sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+
+    return { output, sandboxURL };
   },
 );
